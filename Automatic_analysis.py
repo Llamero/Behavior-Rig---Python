14fdd78cab672244a02c0b3206ecc1169c135f62
Llamero <benjamin.smith@maine.edu>
2019-04-23 01:58:49 -0700

    GUI backbone added

--- Automatic_analysis.py
+++ Automatic_analysis.py
@@ -3,9 +3,8 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 from re import search
-from tkinter import Tk, constants
+from tkinter import Tk, constants, Label, Button, font, Scrollbar, Frame
 from tkinter.ttk import Treeview
-import os
 
 class driveFile:
     def __init__(self): #Initializing contructor
@@ -44,7 +43,6 @@ class driveFile:
         for file in file_list:
             test_name = file['title']
             if search(file_pattern, test_name):
-                print('title: %s, id: %s' % (file['title'], file['id']))
                 subdir_list.append({"Title": file['title'], "mimeType": file['mimeType'], "ID": file['id']})
 
         return subdir_list
@@ -52,20 +50,49 @@ class driveFile:
 class GUI:
     def __init__(self): #Based on treewalk code here: https://stackoverflow.com/questions/14404982/python-gui-tree-walk, and demo: https://stackoverflow.com/questions/36120426/tkinter-treeview-widget-inserting-data
         self.root = Tk() #Create GUI object
-        self.tree = Treeview(columns=("MIME Type", "ID")) #Create Treeview widget - columns - set column names, displaycolumns - specifies which columns are shown
+        self.frame_list = []
+
+        self.root.title("Google Drive")
+
+        #Create treeview widget for file directory
+        self.frame_list.append(Frame(master=self.root)) #Create new frame for status and button
+        self.frame_list[len(self.frame_list)-1].pack(fill=constants.BOTH, expand=True, side=constants.TOP)
+        self.tree = Treeview(self.frame_list[len(self.frame_list)-1], columns=("MIME Type", "ID"), height=20) #Create Treeview widget - columns - set column names, displaycolumns - specifies which columns are shown
+        self.tree["displaycolumns"]=("MIME Type",) #Show only the title and type column, hide the ID column - the trailing column is needed
         self.tree.heading('#0', text='Title')
         self.tree.heading('#1', text='MIME Type')
-        self.tree.heading('#2', text='ID')
-        self.tree.column('#1', stretch=constants.YES)
-        self.tree.column('#2', stretch=constants.YES)
-        self.tree.column('#0', stretch=constants.YES)
-        self.tree.pack(fill='both', expand=True) #Fill - fill given space available, expand - expand - fill whitespace in master tk object
+        #self.tree.heading('#2', text='ID')
+        self.tree.column('#1', stretch=constants.NO, width=450)
+        #self.tree.column('#2', stretch=constants.YES)
+        self.tree.column('#0', stretch=constants.YES, width=450)
+        self.tree.pack(fill=constants.BOTH, expand=True, side=constants.LEFT) #Fill frame when window is resized
+
+        #Add scroll bar to tree
+        self.tree_scroll = Scrollbar(self.frame_list[len(self.frame_list)-1], command=self.tree.yview)
+        self.tree_scroll.pack(side=constants.RIGHT, padx=5, pady=5, fill=constants.Y)
+        self.tree.config(yscrollcommand=self.tree_scroll.set) #Scroll bar has to be set to be controlled by treeview
+
+        #Add status widget
+        self.frame_list.append(Frame(master=self.root)) #Create new frame for status and button
+        self.frame_list[len(self.frame_list)-1].pack(fill=constants.BOTH, expand=False, side=constants.TOP)
+        self.title_label = Label(self.frame_list[len(self.frame_list)-1], text = "Please select file:", anchor=constants.W)
+        self.title_label.pack(side=constants.LEFT, padx=5, pady=5)
+        self.title_font = font.Font(font=self.title_label.cget("font"))
+        self.font_size = self.title_font.actual()["size"]
+        self.title_font.configure(size=round(self.font_size*1.2))
+        self.title_label.configure(font=self.title_font)
+
+        #Create button
+        self.gui_button = Button(self.frame_list[len(self.frame_list)-1], text="OK", command=None, font=self.title_font)
+        self.gui_button.pack(side=constants.RIGHT, padx=5, pady=5)
+
+        #Populate tree widget and initialize event monitor
         self.driveDir = driveFile() #Create instance of Google Drive class
         self.createRoot() #Initialize tree with root folder
         self.tree.bind('<<TreeviewOpen>>', self.updateTree) #Update tree upon event
         self.root.mainloop()
 
-
+    #Generate root directory
     def createRoot(self):
         root_data = self.driveDir.getRoot()
         node = self.tree.insert('', 'end', text=root_data[0]["Title"], values=(root_data[0]["mimeType"], root_data[0]["ID"]), open=True) #Parent, index,
@@ -85,17 +112,19 @@ class GUI:
         self.tree.delete(*self.tree.get_children(node))
 
         parent = self.tree.parent(node)
-        print(node_ID)
         child_list = self.driveDir.getDir(node_ID)
         for f in child_list:
             oid = self.tree.insert(node, 'end', text=f["Title"], values=(f["mimeType"], f["ID"]))
             if f["mimeType"] == "application/vnd.google-apps.folder":
                 self.tree.insert(oid, 0, text='dummy')
 
+    def allSubFiles(self, day = 'all'):
+        pass
+
+    def singleFile(self):
+        pass
 
 def main():
-    #test1 = driveFile("a")
-    #test1.getDir()
     treeGUI = GUI()
 
 
