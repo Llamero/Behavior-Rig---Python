5a8f5c7e68fe54c816841c403909b36203bf3500
Llamero <benjamin.smith@maine.edu>
2019-05-16 12:28:47 -0700

    Full frame scrollbar added

--- generate_protocol.py
+++ generate_protocol.py
@@ -7,7 +7,7 @@ import sys #Allows program to exit on completion
 import psutil #Gives access to USB drive mount events
 import time #Gives access to delays and timing
 from PIL import Image, ImageDraw #Draw images and save as PNG
-from tkinter import font, Tk, Label, Entry, Frame, Checkbutton, Text, Scrollbar, Button, DoubleVar, IntVar, Radiobutton #GUI library
+from tkinter import font, Tk, Label, Entry, Frame, Checkbutton, Text, Scrollbar, Button, DoubleVar, IntVar, Radiobutton, Canvas, Widget #GUI library
 from tkinter.ttk import Separator
 from tkinter.constants import *
 import re #REGEX library
@@ -21,6 +21,73 @@ import glob #Search for files in deirectory
 nCages = 4 #Global variable declaring number of cages
 imageWidth = 1366
 imageHeight = 768
+
+#From: https://gist.github.com/novel-yet-trivial/3eddfce704db3082e38c84664fc1fdf8
+class VerticalScrolledFrame:
+    """
+    A vertically scrolled Frame that can be treated like any other Frame
+    ie it needs a master and layout and it can be a master.
+    :width:, :height:, :bg: are passed to the underlying Canvas
+    :bg: and all other keyword arguments are passed to the inner Frame
+    note that a widget layed out in this frame will have a self.master 3 layers deep,
+    (outer Frame, Canvas, inner Frame) so
+    if you subclass this there is no built in way for the children to access it.
+    You need to provide the controller separately.
+    """
+    def __init__(self, master, **kwargs):
+        width = kwargs.pop('width', None)
+        height = kwargs.pop('height', None)
+        bg = kwargs.pop('bg', kwargs.pop('background', None))
+        self.outer = Frame(master, **kwargs)
+
+        self.vsb = Scrollbar(self.outer, orient=VERTICAL)
+        self.vsb.pack(fill=Y, side=RIGHT)
+        self.canvas = Canvas(self.outer, highlightthickness=0, width=width, height=height, bg=bg)
+        self.canvas.pack(side=LEFT, fill=BOTH, expand=True)
+        self.canvas['yscrollcommand'] = self.vsb.set
+        # mouse scroll does not seem to work with just "bind"; You have
+        # to use "bind_all". Therefore to use multiple windows you have
+        # to bind_all in the current widget
+        self.canvas.bind("<Enter>", self._bind_mouse)
+        self.canvas.bind("<Leave>", self._unbind_mouse)
+        self.vsb['command'] = self.canvas.yview
+
+        self.inner = Frame(self.canvas, bg=bg)
+        # pack the inner Frame into the Canvas with the topleft corner 4 pixels offset
+        self.canvas.create_window(4, 4, window=self.inner, anchor='nw')
+        self.inner.bind("<Configure>", self._on_frame_configure)
+
+        self.outer_attr = set(dir(Widget))
+
+    def __getattr__(self, item):
+        if item in self.outer_attr:
+            # geometry attributes etc (eg pack, destroy, tkraise) are passed on to self.outer
+            return getattr(self.outer, item)
+        else:
+            # all other attributes (_w, children, etc) are passed to self.inner
+            return getattr(self.inner, item)
+
+    def _on_frame_configure(self, event=None):
+        x1, y1, x2, y2 = self.canvas.bbox("all")
+        height = self.canvas.winfo_height()
+        self.canvas.config(scrollregion = (0,0, x2, max(y2, height)))
+
+    def _bind_mouse(self, event=None):
+        self.canvas.bind_all("<4>", self._on_mousewheel)
+        self.canvas.bind_all("<5>", self._on_mousewheel)
+        self.canvas.bind_all("<MouseWheel>", self._on_mousewheel)
+
+    def _unbind_mouse(self, event=None):
+        self.canvas.unbind_all("<4>")
+        self.canvas.unbind_all("<5>")
+        self.canvas.unbind_all("<MouseWheel>")
+
+    def _on_mousewheel(self, event):
+        """Linux uses event.num; Windows / Mac uses event.delta"""
+        if event.num == 4 or event.delta > 0:
+            self.canvas.yview_scroll(-1, "units" )
+        elif event.num == 5 or event.delta < 0:
+            self.canvas.yview_scroll(1, "units" )
 
 def buildGUI():
     ####See "def loadPreset()" to change default protocol settings###
@@ -299,8 +366,10 @@ def buildGUI():
             uploadButton.config(text="Upload")
             loadPreset() #Setup GUI to match current preset
 
-    gui = Tk()
-    gui.title("Protocol generator...")
+    mainWindow = Tk()
+    mainWindow.title("Protocol generator...")
+    gui = VerticalScrolledFrame(mainWindow, width=470, height=740)
+    gui.pack(fill=BOTH, expand=True)
 
     #Initialize frame set
     gui.grid_columnconfigure(0, weight=1)
