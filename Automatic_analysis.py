71c9517e6c28f0c05c5f1e12fae7f753a1b28b9d
Llamero <benjamin.smith@maine.edu>
2019-05-21 23:13:27 -0700

    Untested drafting of analysis

--- Automatic_analysis.py
+++ Automatic_analysis.py
@@ -8,6 +8,12 @@ from tkinter.ttk import Treeview
 from collections import OrderedDict
 from time import sleep
 
+import matplotlib.pyplot as plt #plot results
+from statistics import mean, median
+from os import listdir
+import seaborn as sb #plot swarmplot
+from math import inf #Allow for infinity value
+
 class driveFile:
     def __init__(self): #Initializing contructor
         #Authenticate Google Drive connection
@@ -92,7 +98,7 @@ class GUI:
         self.radio_label.configure(font=self.title_font)
         self.day_var = StringVar() #Var to track selected checkbox
         self.day_var.set("Night #1") #Set "All" as default value
-        self.radio_list = OrderedDict((("Night #1", None), ("Night #2", None), ("Night #3", None), ("Night #4", None), ("Refresher", None), ("Test", None), ("All", None))) #List to store radio button widgets
+        self.radio_list = OrderedDict((("Night #1", None), ("Night #2", None), ("Night #3", None), ("Night #4", None), ("Refresher", None), ("Contrast", None), ("All", None))) #List to store radio button widgets
         for text in self.radio_list.keys():
             self.b = Radiobutton(self.frame_list["Radio"], text=text, variable=self.day_var, value=text, font=self.title_font, command=None)
             self.b.pack(fill=constants.BOTH, side=constants.LEFT, expand=True)
@@ -218,33 +224,207 @@ class GUI:
                             print(genotype + " " + run_number + " " + day + " " + preset_day + " " + cage)
                             break
 
+            #If node is a folder, recursively continue down the file tree, retrieveing folder metadata if available
             elif(self.tree.set(node, "MIME Type") in "application/vnd.google-apps.folder"):
-                if(", Starting " in node_title):
+                if(", Starting " in node_title): #Get genotype and run number metadata if at a node with this information
                     #Extract genotpye metadata
                     genotype = node_title.split(",")[0]
                     genotype, run_number = genotype.split("-")
 
-                elif(self.day_var.get() in node_title):
+                elif(self.day_var.get() in node_title): #Get day metadata if at a node with this information
                     day = node_title
 
                 else:
                     pass
 
+                #If the nodelist contains a child node, recursively go to this node.  Otherwise, go to all children nodes of the current node
                 try:
-                    node_list[node_index+1]
+                    node_list[node_index+1] #Check if child node is available - throws exception if not
                     self.getFiles(node_list, node_index+1, genotype, run_number, day)
                 except:
-                    self.fillTree(node)
+                    self.fillTree(node) #Download all child nodes, and then check all child nodes
                     for child in self.tree.get_children(node):
                         new_node_list = node_list + [child]
                         self.getFiles(new_node_list, node_index+1, genotype, run_number, day)
             else:
                 pass
 
+class analysis:
+    def __init__(self, day, file_dic):
+        self.file_dic = file_dic
+        self.day = day
+        self.bin_colors = [[1,0,0],[1,1,0],[0,1,0]]
+
+        #Retrieve number of files in file_dic
+        self.n_files = 0
+        for genotype in file_dic.keys():
+            for run in file_dic[genotype].keys():
+                for day in file_dic[genotype][run].keys():
+                    for cage in file_dic[genotype][run][day].keys():
+                        self.n_files += 1
+
+        self.day_func = {"Day #1": [],
+                        "Day #2": [],
+                        "Day #3": [],
+                        "Day #4": [],
+                        "Refresher": [],
+                        "Contrast": [],
+                        "All": []}
+        try:
+            for func in day_func[self.day]:
+                func()
+        except:
+            pass
+
+    def individualSuccessRate(self):
+        def analyzeData(self, file, metadata):
+            #Lists for data characterization
+            control_wheel_interval = [] #Time between end of reward and first subsequent wheel event
+            reward_door_interval = [] #Time between reward start and first door open
+            door_with_pump_on = [] #TIme of door open event while pump was active
+            door_with_pump_off_before_wheel = [] #Time of door open event after pump is off, but before subsequent wheel event
+            door_with_pump_off_after_wheel = [] #Time of door open event after pump is off and subsequent first wheel event
+            door_event_duration_reward = [] #Duration of each door open event during reward and before first wheel
+            door_event_duration_control = [] #Duration of each door open event during control image after first wheel
+            revolutions_before_reset = [] #Number of wheel revolutions when wheel counter was reset
+            wheel_overshoot = [] #Number of extra wheel revolutions after reward is active
+            wheel_interval = [] #Time between wheel events - track mouse speed and activity
+            wheel_event_time = [] #Time of each wheel event
+            door_open_event_time = [] #Time of each door open event
+
+            #Tracking variables for data analysis
+            first_wheel = False #First wheel event after prev reward has occured - reset to false on start of reward
+            reward_active = False #If reward image is active
+            pump_on = False #Whether pump is on
+            overshoot_counter = 0 #Number of overshoot events during current reward event - reset to 0 on control image
+            prev_revolution = 0 #Number of revolutions during control - reset to 0 on reward image
+            prev_wheel_event = 0 #Time of previous wheel event
+            door_open_time = 0 #Time of door open event
+            door_closed_time = 0 #Time of door closed event
+            reward_time = None #Time that reward state last changed
+            control_images = []
+            reward_images = []
+
+            for line in file:
+                if line.startswith("Wheel - State: High, Time: "):
+                    wheel_time = float(search(r", Time: \d+\.\d+", line).group(0)[8:])
+                    wheel_event_time.append(wheel_time)
+                    wheel_interval = wheel_time - prev_wheel_event
+                    prev_wheel_event
+                    if(not(reward_active or first_wheel)):
+                        first_wheel = True
+                        control_wheel_interval.append(wheel_time-reward_time)
+                    elif(reward_active):
+                        overshoot_counter += 1
+                elif line.startswith("Wheel - State: Low, Time: "):
+                    pass
+                elif line.startswith("Wheel revolution "):
+                    current_revolution = int(search(r"Wheel revolution \d+", line).group(0)[17:])
+                    if(current_revolution <= prev_revolution):
+                        revolutions_before_reset.append(prev_revolution)
+                    prev_revolution = current_revolution
+                elif line.startswith("Door - State: Low, "):
+                    door_open_time = float(search(r", Time: \d+\.\d+", line).group(0)[8:])
+                    door_open_event_time.append(door_open_time)
+                    if(reward_active):
+                        door_with_pump_on.append(door_open_time)
+                        if(not pump_on):
+                            reward_door_interval.append(door_open_time - reward_time)
+                            wheel_overshoot.append(overshoot_counter)
+                    else:
+                        if(first_wheel):
+                            door_with_pump_off_after_wheel.append(door_open_time)
+                        else:
+                            door_with_pump_off_before_wheel.append(door_open_time)
+                elif line.startswith("Door - State: High, "):
+                    door_closed_time = float(search(r", Time: \d+\.\d+", line).group(0)[8:])
+                    door_duration = door_closed_time-door_open_time
+                    if(first_wheel):
+                        door_event_duration_control.append(door_duration)
+                    else:
+                        door_event_duration_reward.append(door_duration)
+                elif line.startswith("Image - Name: "):
+                    image_name = search(r"Image - Name: [^,]+\.png", line).group(0)[14:]
+                    reward_time = float(search(r", Time: \d+\.\d+", line).group(0)[8:])
+                    if(image_name in control_images):
+                        reward_active = False
+                        overshoot_counter = 0
+                    elif(image_name in reward_images):
+                        reward_active = True
+                        prev_revolution = 0
+                        first_wheel = False
+                    else:
+                        print("ERROR: Invalid image.")
+                elif line.startswith("Pump - State: On, Time: "):
+                    pass
+                elif line.startswith("Pump - State: Off, Time: "):
+                    pass
+                elif not (control_images and reward_images):
+                    if("Control image set: [" in line):
+                        list_string = search(r"Control image set: \[[^\]]*\]", line).group(0)[20:-1]
+                        control_images = list_string.split(", ")
+                    if("Reward image set: [" in line):
+                        list_string = search(r"Reward image set: \[[^\]]*\]", line).group(0)[19:-1]
+                        reward_images = list_string.split(", ")
+                    pass
+                else:
+                    pass
+            return {"control wheel interval": control_wheel_interval, #Time between end of reward and first subsequent wheel event
+                    "reward_door_interval": reward_door_interval, #Time between reward start and first door open
+                    "door with pump on": door_with_pump_on, #TIme of door event while pump was active
+                    "door with pump off before wheel": door_with_pump_off_before_wheel, #Time of door event after pump is off, but before subsequent wheel event
+                    "door with pump off after wheel": door_with_pump_off_after_wheel, #Time of door event after pump is off and subsequent first wheel event
+                    "door event duration reward": door_event_duration_reward, #Duration of each door open event during reward and before first wheel
+                    "door event duration control": door_event_duration_control, #Duration of each door open event during control image after first wheel
+                    "revolutions before reset": revolutions_before_reset, #Number of wheel revolutions when wheel counter was reset
+                    "wheel_overshoot": wheel_overshoot, #Number of extra wheel revolutions after reward is active
+                    "wheel interval": wheel_interval, #Time between wheel events - track mouse speed and activity
+                    "wheel event time": wheel_event_time, #Time of each wheel event
+                    "door open event time": door_open_event_time} #Time of each door open event
+
+        raster_list = []
+        x_label_list = []
+        for genotype in file_dic.keys():
+            for run in file_dic[genotype].keys():
+                for day in file_dic[genotype][run].keys():
+                    for cage, file in file_dic[genotype][run][day].items():
+                        analysis_dic = raster_list(file, {"genotype": genotype, "run": run, "day": day, "cage": cage})
+                        raster_list.append([analysis_dic["door with pump on"], analysis_dic["door with pump off before wheel"], analysis_dic["door with pump off after wheel"]])
+                        x_label_string = generateSummaryString(analysis_dic)
+                        x_label_list.append(x_label_string)
+
+        self.plotRaster(raster_array, ["Between wheel events", "After reward and before wheel", "During active reward"], True, x_label_list)
+
+    def plotRaster(self, raster_array, raster_bins, stagger_rasters, x_labels):
+        total_line_width = 0.8 #Fraction of 1
+        n_samples = len(raster_array)/len(self.bin_colors)
+        raster_colors = self.bin_colors*(n_samples) #Assign a line color for each element in raster array
+        raster_offsets = [int(i/len(self.bin_colors))+1 for i in range(len(raster_colors))] #Stack all bins per mouse/day
+        raster_line_length = [total_line_width]*len(raster_colors) #Make line widths the default line width
+        if stagger_rasters:
+            new_line_width = total_line_width/len(raster_bins) #Divide total line width across all bins
+            offset = 0-(total_line_width)/2
+            for a in range(len(raster_array)):
+                 raster_line_length = newLineWidth
+                 sub_bin = a%len(self.bin_colors) #
+                 n_bin = int(a/len(self.bin_colors))+1
+                 raster_offsets[a] = n_bin + offset + new_line_width*(sub_bin+0.5)
+
+        if(x_labels):
+            x_tick = list(range(1,n_samples+1))
+            plt.xticks(x_tick, x_labels)
+            plt.tight_layout() #Autoscale window to remove overlap
+            figWindow = plt.gcf().get_size_inches()
+            figWindow[0] = figWindow[0]*1.2
+            figWindow[1] = figWindow[1]*1.2
+            fig.set_size_inches(figWindow[0], figWindow[1])
+
+
+
+
+
 def main():
     treeGUI = GUI()
-
-
 
 if __name__ == '__main__':
     main()
